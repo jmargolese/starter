2a33135225b05abe28fd84650fdd6a521708f2fa
stansongman <stanswin@gmail.com>
2017-11-15 16:16:01 -0500

    Adds test stripe account to seeds

---
+++ src/seeds/seedStripeAccounts.ts
@@ -0,0 +1,21 @@
+export const stripeAccountSeeds = 
+[
+	{
+		key : "OrgHumane",
+		data : {
+			stripeAcctId : "acct_1Ai6pMHMFCaEtegi"
+		}
+	},
+	{
+		key : "OrgChadTough",
+		data : {
+			stripeAcctId : "acct_1Ai6pMHMFCaEtegi"
+		}
+	},
+	{
+		key : "OrgDesireStreet",
+		data : {
+			stripeAcctId : "acct_1Ai6pMHMFCaEtegi"
+		}
+	}
+]
