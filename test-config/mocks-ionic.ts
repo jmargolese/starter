9d1e73ac2a56e5adaaa86044d07b84bc13b2eef1
skavsan <skavsan@ipaymyway.com>
2017-11-18 12:08:14 -0500

    unit testing cleanup

--- test-config/mocks-ionic.ts
+++ test-config/mocks-ionic.ts
@@ -118,141 +118,132 @@ export class NavMock {
 
 export class ViewControllerMock {
   
-      public readReady: any = {
-          emit(): void {
-  
-          },
-          subscribe(): any {
-  
-          }
-      };
-  
-      public writeReady: any = {
-          emit(): void {
-  
-          },
-          subscribe(): any {
-  
-          }
-      };
-  
-      public contentRef(): any {
-          return new Promise(function (resolve: Function): void {
-              resolve();
-          });
-      }
-  
-      public didEnter(): any {
-          return new Promise(function (resolve: Function): void {
-              resolve();
-          });
-      }
-  
-      public didLeave(): any {
-          return new Promise(function (resolve: Function): void {
-              resolve();
-          });
-      }
-  
-      public onDidDismiss(): any {
-          return new Promise(function (resolve: Function): void {
-              resolve();
-          });
-      }
-  
-      public onWillDismiss(): any {
-          return new Promise(function (resolve: Function): void {
-              resolve();
-          });
-      }
-  
-      public willEnter(): any {
-          return new Promise(function (resolve: Function): void {
-              resolve();
-          });
-      }
-  
-      public willLeave(): any {
-          return new Promise(function (resolve: Function): void {
-              resolve();
-          });
-      }
-  
-      public willUnload(): any {
-          return new Promise(function (resolve: Function): void {
-              resolve();
-          });
-      }
-  
-      public dismiss(): any {
-          return true;
-      }
-  
-      public enableBack(): any {
-          return true;
-      }
-  
-      public getContent(): any {
-          return true;
-      }
-  
-      public hasNavbar(): any {
-          return true;
-      }
-  
-      public index(): any {
-          return true;
-      }
-  
-      public isFirst(): any {
-          return true;
-      }
-  
-      public isLast(): any {
-          return true;
-      }
-  
-      public pageRef(): any {
-          return true;
-      }
-  
-      public setBackButtonText(): any {
-          return true;
-      }
-  
-      public showBackButton(): any {
-          return true;
-      }
-  
-      public _setHeader(): any {
-          return true;
-      }
-  
-      public _setIONContent(): any {
-          return true;
-      }
-  
-      public _setIONContentRef(): any {
-          return true;
-      }
-  
-      public _setNavbar(): any {
-          return true;
-      }
-  
-      public _setContent(): any {
-          return true;
-      }
-  
-      public _setContentRef(): any {
-          return true;
-      }
-  
-      public _setFooter(): any {
-          return true;
-      }
-  
+  public readReady: any = {
+    emit(): void {},
+    subscribe(): any {}
+  };
+
+  public writeReady: any = {
+    emit(): void {},
+    subscribe(): any {}
+  };
+
+  public contentRef(): any {
+    return new Promise(function (resolve: Function): void {
+        resolve();
+    });
   }
+
+  public didEnter(): any {
+    return new Promise(function (resolve: Function): void {
+        resolve();
+    });
+  }
+
+  public didLeave(): any {
+    return new Promise(function (resolve: Function): void {
+        resolve();
+    });
+  }
+
+  public onDidDismiss(): any {
+    return new Promise(function (resolve: Function): void {
+        resolve();
+    });
+  }
+
+  public onWillDismiss(): any {
+    return new Promise(function (resolve: Function): void {
+        resolve();
+    });
+  }
+
+  public willEnter(): any {
+    return new Promise(function (resolve: Function): void {
+        resolve();
+    });
+  }
+
+  public willLeave(): any {
+    return new Promise(function (resolve: Function): void {
+        resolve();
+    });
+  }
+
+  public willUnload(): any {
+    return new Promise(function (resolve: Function): void {
+        resolve();
+    });
+  }
+
+  public dismiss(): any {
+    return true;
+  }
+
+  public enableBack(): any {
+    return true;
+  }
+
+  public getContent(): any {
+    return true;
+  }
+
+  public hasNavbar(): any {
+    return true;
+  }
+
+  public index(): any {
+    return true;
+  }
+
+  public isFirst(): any {
+    return true;
+  }
+
+  public isLast(): any {
+    return true;
+  }
+
+  public pageRef(): any {
+    return true;
+  }
+
+  public setBackButtonText(): any {
+    return true;
+  }
+
+  public showBackButton(): any {
+    return true;
+  }
+
+  public _setHeader(): any {
+    return true;
+  }
+
+  public _setIONContent(): any {
+    return true;
+  }
+
+  public _setIONContentRef(): any {
+    return true;
+  }
+
+  public _setNavbar(): any {
+    return true;
+  }
+
+  public _setContent(): any {
+    return true;
+  }
+
+  public _setContentRef(): any {
+    return true;
+  }
+
+  public _setFooter(): any {
+    return true;
+  }
+}
 
 export class StripeMock {}
 export class AlertProviderMock {}
@@ -289,17 +280,16 @@ export class AngularFirestoreMock {
   public collection() {
     return new Promise( (resolve, reject) => { 
       resolve(this.b)
-    });
+    })
   }
 }
-
 
 export class OrganizationProviderMock {
   organization = 'mockedOrganization';
   public getAllOrganization() {
     return new Promise( (resolve, reject) => {
       resolve(this.organization)
-    });
+    })
   }  
 }
 
@@ -308,7 +298,7 @@ export class NotificationsProviderMock {
   public init() {
     return new Promise( (resolve, reject) => {
       resolve(this.b)
-    });
+    })
   }   
 }
 
@@ -321,21 +311,22 @@ export class AppVersionMock {
   public getVersionNumber() {
     return new Promise( (resolve, reject) => {
       resolve(this.version)
-    });
+    })
   }
   public getAppName() {
     return new Promise( (resolve, reject) => {
       resolve(this.name)
-    });
+    })
   }
   public getPackageName() {
     return new Promise( (resolve, reject) => {
       resolve(this.package)
-    });
+    })
   }
+
   public getVersionCode() {
     return new Promise( (resolve, reject) => {
       resolve(this.code)
-    });
+    })
   }
 }
