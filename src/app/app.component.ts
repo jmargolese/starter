7b8e35723a66ea3b26eedab65ef24e9806ab060a
stansongman <stanswin@gmail.com>
2017-11-29 10:58:36 -0500

    adds build:local capability to package.json, adds logging of mode and release at startup app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { ENV } from '@app/env';
 
 
 
@@ -15,6 +15,7 @@ export class Share {
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
+      console.log(`Shareapp is ready.  Mode: ${ENV.mode} Release: ${ENV.release}`);
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
